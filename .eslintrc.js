e2e7e1a07c8759af530cd89a707dba019a0d68d3
inuscript <inuscript@gmail.com>
2017-01-30 23:08:26 +0900

    error pattern

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   "parser": "typescript-eslint-parser",
   // "parser": "babel-eslint",
-  "env": {
-   "node": true,
-   "es6": true
-  },
   "parserOptions": {
     "ecmaVersion": 7,
     "sourceType": "module",
@@ -13,18 +9,7 @@ module.exports = {
       "experimentalObjectRestSpread": true
     },
   },
-  "extends": [
-    "eslint:recommended",
-  ],
-  "plugins": [
-    "react"
-  ],
   "rules": {
-    // "no-empty-character-class": 2
-    // "semi": [2, "never"],
-    "react/jsx-uses-vars": 2,
-    "react/jsx-uses-react": 2,
-    "react/jsx-tag-spacing": [2],
-    // "react/jsx-curly-spacing": [2, "always"],
+    "no-unused-vars": 2
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-import React from "react"
-
-export const Item = () => (
-  <div>foo</div>
-)
-
-const a = {}
-export const actions = {
-  ...a,
-}
 
 import { schema } from "normalizr"
 
